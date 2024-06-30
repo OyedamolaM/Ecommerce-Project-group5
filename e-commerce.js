be7f6129f3fb224e166d88805f6dea2d80e4e3e6
Oyedamola Moreira <66420203+OyedamolaM@users.noreply.github.com>
2024-07-01 00:21:24 +0100

    rendered javascript for the new arrival page

--- e-commerce.js
+++ e-commerce.js
@@ -1,6 +1,6 @@
 
 const products = [{
-    image: 'assets/image 7.png,',
+    image: 'assets/image 7.png',
     name: 'T-Shirt with tape details',
     rating: {
         stars: 4.5,
@@ -11,23 +11,83 @@ const products = [{
 },
 {
     image: 'assets/skinny fit jeans.png',
-    name: 'Skinny-fit Jeans ',
+    name: 'Skinny-fit Jeans',
     rating: {
         stars: 4,
         count: 61,
     },
     priceCents: 1090
 
-}
+},
+{
+    image: 'assets/Checkered Shirt.png',
+    name: 'Checkered Shirt',
+    rating: {
+        stars: 4,
+        count: 61,
+    },
+    priceCents: 1090
+
+},
+{
+    image: 'assets/Sleeve Striped T-Shirt.png',
+    name: 'Sleeve Striped T-Shirt',
+    rating: {
+        stars: 4,
+        count: 61,
+    },
+    priceCents: 1090
+
+},
+{
+    image: 'assets/Vertical Striped Shirt.png',
+    name: 'Vertical-Striped Shirt',
+    rating: {
+        stars: 4,
+        count: 61,
+    },
+    priceCents: 1090
+
+},
+{
+    image: 'assets/Courage Graphic T-shirt.png',
+    name: 'Courage Graphic T-Shirt',
+    rating: {
+        stars: 4,
+        count: 61,
+    },
+    priceCents: 1090
+
+},
+{
+    image: 'assets/bermuda shorts.png',
+    name: 'Loose-fit Bermuda Shorts',
+    rating: {
+        stars: 4,
+        count: 61,
+    },
+    priceCents: 1090
+
+},
+{
+    image: 'assets/Faded Skinny jeans.png',
+    name: 'Faded Skinny Jeans',
+    rating: {
+        stars: 4,
+        count: 61,
+    },
+    priceCents: 1090
+
+},
 ]
 
 let productsHTML = "";
 
 products.forEach((product) => {
- productsHTML +=`
- <div class="thumbnail">
-              <!-- <a href="/w3images/fjords.jpg" target="_blank"> -->
-                <img src="${product.image}" style="width:100%; height:300px" class="rounded-3">
+    productsHTML +=`
+    <div class="col-md-3">
+            <div class="thumbnail">
+                <img src='${product.image}' alt="tape details" style="width:100%; height:300px" class="rounded-3">
                 <div class="row">
                     <div class="col-md-12">
                      <span class="label label-primary">${product.name}</span>
@@ -50,7 +110,36 @@ products.forEach((product) => {
                     </div>
               <!-- </a> -->
             </div>
-            </div>`
+            </div>
+          </div>
+    `
+//  productsHTML +=`
+//     <div class="col-md-3">
+//         <div class="thumbnail">
+//                 <img src="${product.image}" style="width:100%; height:300px" class="rounded-3">
+//                 <div class="row">
+//                     <div class="col-md-12">
+//                      <span class="label label-primary">${product.name}</span>
+//                      <div class="row">
+//                         <div class="col-md-8" style="width: 100;">
+//                          <span class="sr-only">Four out of Five Stars</span>
+//                          <span class="glyphicon glyphicon-star"></span>
+//                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+//                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+//                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+//                          <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+//                          <span class="label label-success">${product.rating.count}</span>
+//                         </div>
+//                        </div>
+//                        <div class="row">
+//                         <div class="col-md-6">
+//                             <h5 class="product-price">${product.priceCents /100}</h5>
+//                         </div>
+//                        </div>
+//                     </div>
+//             </div>
+//             </div>
+//             </div>`
 
             
 })
