b427425b6ba4cdd11b7f39d85003431ace962d7c
Oyedamola Moreira <66420203+OyedamolaM@users.noreply.github.com>
2024-07-01 05:22:40 +0100

    rendered javascript for top selling page

--- e-commerce.js
+++ e-commerce.js
@@ -1,5 +1,6 @@
 
-const products = [{
+//products list for New Arrivals section
+const newArrivals = [{
     image: 'assets/image 7.png',
     name: 'T-Shirt with tape details',
     rating: {
@@ -39,7 +40,49 @@ const products = [{
     priceCents: 1090
 
 },
-{
+]
+
+let productsHTML = "";
+// rendering of New Arrivals section
+newArrivals.forEach((product) => {
+    productsHTML +=`
+    <div class="col-md-3">
+            <div class="thumbnail">
+                <img src='${product.image}' alt="tape details" style="width:100%; height:300px" class="rounded-3">
+                <div class="row">
+                    <div class="col-md-12">
+                     <span class="label label-primary">${product.name}</span>
+                     <div class="row">
+                        <div class="col-md-8" style="width: 100;">
+                         <span class="sr-only">Four out of Five Stars</span>
+                         <span class="glyphicon glyphicon-star"></span>
+                         <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                         <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                         <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                         <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+                         <span class="label label-success">${product.rating.count}</span>
+                        </div>
+                       </div>
+                       <div class="row">
+                        <div class="col-md-6">
+                            <h5 class="product-price">${product.priceCents /100}</h5>
+                        </div>
+                       </div>
+                    </div>
+            </div>
+            </div>
+          </div>
+    `
+//  
+
+            
+})
+console.log(productsHTML);
+
+document.querySelector('.new-arrivals-grid').innerHTML = productsHTML; 
+
+
+const topSelling = [{
     image: 'assets/Vertical Striped Shirt.png',
     name: 'Vertical-Striped Shirt',
     rating: {
@@ -81,10 +124,10 @@ const products = [{
 },
 ]
 
-let productsHTML = "";
-
-products.forEach((product) => {
-    productsHTML +=`
+let topSellingHTML = "";
+// rendering of New Arrivals section
+topSelling.forEach((product) => {
+    topSellingHTML +=`
     <div class="col-md-3">
             <div class="thumbnail">
                 <img src='${product.image}' alt="tape details" style="width:100%; height:300px" class="rounded-3">
@@ -113,38 +156,12 @@ products.forEach((product) => {
             </div>
           </div>
     `
-//  productsHTML +=`
-//     <div class="col-md-3">
-//         <div class="thumbnail">
-//                 <img src="${product.image}" style="width:100%; height:300px" class="rounded-3">
-//                 <div class="row">
-//                     <div class="col-md-12">
-//                      <span class="label label-primary">${product.name}</span>
-//                      <div class="row">
-//                         <div class="col-md-8" style="width: 100;">
-//                          <span class="sr-only">Four out of Five Stars</span>
-//                          <span class="glyphicon glyphicon-star"></span>
-//                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-//                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-//                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-//                          <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-//                          <span class="label label-success">${product.rating.count}</span>
-//                         </div>
-//                        </div>
-//                        <div class="row">
-//                         <div class="col-md-6">
-//                             <h5 class="product-price">${product.priceCents /100}</h5>
-//                         </div>
-//                        </div>
-//                     </div>
-//             </div>
-//             </div>
-//             </div>`
+//  
 
             
 })
-console.log(productsHTML);
+console.log(topSellingHTML);
 
-document.querySelector('.js-product-grid').innerHTML = productsHTML; 
+document.querySelector('.top-selling').innerHTML = topSellingHTML; 
 
 
