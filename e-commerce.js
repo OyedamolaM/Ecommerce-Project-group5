887a7442a0963b021bf3d86017cde5216307fdcd
Oyedamola Moreira <66420203+OyedamolaM@users.noreply.github.com>
2024-07-01 06:23:31 +0100

    created a new folder called data, created a new file for product data, copied out the products array away from the ecommerce.js into the new product data js file, and then added the product data in a new script tag withinthe html

--- e-commerce.js
+++ e-commerce.js
@@ -1,51 +1,7 @@
-
-//products list for New Arrivals section
-const newArrivals = [{
-    image: 'assets/image 7.png',
-    name: 'T-Shirt with tape details',
-    rating: {
-        stars: 4.5,
-        count: 87,
-    },
-    priceCents: 1090
-
-},
-{
-    image: 'assets/skinny fit jeans.png',
-    name: 'Skinny-fit Jeans',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
-
-},
-{
-    image: 'assets/Checkered Shirt.png',
-    name: 'Checkered Shirt',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
-
-},
-{
-    image: 'assets/Sleeve Striped T-Shirt.png',
-    name: 'Sleeve Striped T-Shirt',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
-
-},
-]
-
-let productsHTML = "";
+let NewArrivalHTML = "";
 // rendering of New Arrivals section
 newArrivals.forEach((product) => {
-    productsHTML +=`
+    NewArrivalHTML +=`
     <div class="col-md-3">
             <div class="thumbnail">
                 <img src='${product.image}' alt="tape details" style="width:100%; height:300px" class="rounded-3">
@@ -65,9 +21,14 @@ newArrivals.forEach((product) => {
                        </div>
                        <div class="row">
                         <div class="col-md-6">
-                            <h5 class="product-price">${product.priceCents /100}</h5>
+                            <h5 class="product-price">$${(product.priceCents /100).toFixed(2)}</h5>
                         </div>
                        </div>
+                       <div class="row">
+                            <div class="col-md-12">
+                                <button>Add to cart</button>
+                            </div>
+                        </div>
                     </div>
             </div>
             </div>
@@ -77,55 +38,14 @@ newArrivals.forEach((product) => {
 
             
 })
-console.log(productsHTML);
+console.log(NewArrivalHTML);
 
-document.querySelector('.new-arrivals-grid').innerHTML = productsHTML; 
+document.querySelector('.new-arrivals-grid').innerHTML = NewArrivalHTML; 
 
 
-const topSelling = [{
-    image: 'assets/Vertical Striped Shirt.png',
-    name: 'Vertical-Striped Shirt',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
-
-},
-{
-    image: 'assets/Courage Graphic T-shirt.png',
-    name: 'Courage Graphic T-Shirt',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
-
-},
-{
-    image: 'assets/bermuda shorts.png',
-    name: 'Loose-fit Bermuda Shorts',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
-
-},
-{
-    image: 'assets/Faded Skinny jeans.png',
-    name: 'Faded Skinny Jeans',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
-
-},
-]
 
 let topSellingHTML = "";
-// rendering of New Arrivals section
+// rendering of New Top selling section
 topSelling.forEach((product) => {
     topSellingHTML +=`
     <div class="col-md-3">
@@ -144,14 +64,19 @@ topSelling.forEach((product) => {
                          <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
                          <span class="label label-success">${product.rating.count}</span>
                         </div>
-                       </div><!-- end row -->
+                       </div>
+                    
                        <div class="row">
                         <div class="col-md-6">
-                            <h5 class="product-price">${product.priceCents /100}</h5>
+                            <h5 class="product-price">$${product.priceCents /100}</h5>
                         </div>
                        </div>
+                       <div class="row">
+                            <div class="col-md-12">
+                                <button>Add to cart</button>
+                            </div>
+                        </div>
                     </div>
-              <!-- </a> -->
             </div>
             </div>
           </div>
