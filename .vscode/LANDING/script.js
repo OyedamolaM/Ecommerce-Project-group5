5249ca555158bf69ab688591c4fc0076606f53af
sekem12345 <akinolaitunu12345@gmail.com>
2024-07-20 18:38:35 +0100

    some changes
    changes

--- .vscode/LANDING/script.js
+++ .vscode/LANDING/script.js
@@ -1,87 +1,119 @@
-
- let ArrProducts =[
+let ArrProducts = [
   {
-
-    id:1,
+    id: 1,
     name: "HTML",
-    image:"ada.jpg",
-    price:"1000",
-    rating:5,
+    image: "/images/2.jpg",
+    price: "1000",
+    rating: 5,
   },
   {
-
-    id:2,
+    id: 2,
     name: "CSS",
-    image:"image/ahii.jpg",
-    price:"1000",
-    rating:5,
+    image: "/images/ahii.jpg",
+    price: "1000",
+    rating: 5,
   },
   {
-
-    id:1,
-    name: "HTML",
-    image:"images/istockphoto-1330867859-612x612.jpg",
-    price:"1000",
-    rating:5,
+    id: 3,
+    name: "JavaScript",
+    image: "images/istockphoto-1330867859-612x612.jpg",
+    price: "1000",
+    rating: 5,
   },
   {
-
-    id:1,
-    name: "HTML",
-    image:"images/istockphoto-1330867859-612x612.jpg",
-    price:"1000",
-    rating:5,
+    id: 4,
+    name: "React",
+    image: "images/istockphoto-1330867859-612x612.jpg",
+    price: "1000",
+    rating: 5,
   },
   {
-
-    id:1,
-    name: "HTML",
-    image:"images/istockphoto-1330867859-612x612.jpg",
-    price:"1000",
-    rating:5,
+    id: 5,
+    name: "Node.js",
+    image: "images/istockphoto-1330867859-612x612.jpg",
+    price: "1000",
+    rating: 5,
   },
   {
-
-    id:1,
-    name: "HTML",
-    image:"images/istockphoto-1330867859-612x612.jpg",
-    price:"1000",
-    rating:5,
+    id: 6,
+    name: "Express",
+    image: "images/istockphoto-1330867859-612x612.jpg",
+    price: "1000",
+    rating: 5,
   },
-
- ]
-
-
- const body = document.querySelector("body");
- products = document.querySelector(".products");
-
-let checkOutList =[
-
 ];
-  function onInIt() {
-    ArrProducts.forEach((item, key) => {
-      
-      let div = document.createElement("div");
-         div.classList.add('item');
 
-       div.innerHTML=`
-       <img src="image/${item.image}" />;
-       <div class="name">${item.name}</div>
-       <div class="price">$<small> ${item.price}</div>
-       <button onclick="addToCart(${key})">add to cart </button> 
-       `;
+const productsContainer = document.querySelector(".products");
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const closeModalBtn = document.querySelector(".close");
 
-       products.appendChild(div);
+// Initialize products in localStorage (for testing purposes)
+localStorage.setItem('products', JSON.stringify(ArrProducts));
+
+// Retrieve products from localStorage
+const NewProduct = JSON.parse(localStorage.getItem('products'));
+
+function onInIt() {
+  NewProduct.forEach((item, key) => {
+    let div = document.createElement("div");
+    div.classList.add('item');
+    div.innerHTML = `
+      <img src="${item.image}" />
+      <div class="name">${item.name}</div>
+      <div class="price">$<small>${item.price}</small></div>
+    `;
+    
+    // Add event listener to show modal on click
+    div.addEventListener('click', () => {
+      showModal(item);
     });
-  }
-  onInIt();
 
-  
-  function addToCart(Id) {
-    console.log(ArrProducts[Id]);
-if (checkOutList [Id] == null){
-  checkOutList [Id]  = ArrProducts[Id];
-  
+    productsContainer.appendChild(div);
+  });
 }
+
+function showModal(product) {
+  // Populate modal with product details
+  document.getElementById('modal-name').textContent = product.name;
+  document.getElementById('modal-image').src = product.image;
+  document.getElementById('modal-price').textContent = `Price: $${product.price}`;
+
+  // Display modal
+  modal.style.display = "block";
+
+  // Close modal when close button is clicked
+  closeModalBtn.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // Close modal when clicking outside of the modal content
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+// Initialize product display
+onInIt();
+
+// Function to add a new product (testing purpose)
+function addProduct() {
+  const newProduct = {
+    id: ArrProducts.length + 1,
+    name: 'New Product',
+    price: '2000',
+    image: 'images/new-product.jpg'
   };
+  
+  ArrProducts.push(newProduct);
+  localStorage.setItem('products', JSON.stringify(ArrProducts));
+  
+  // Refresh product display
+  productsContainer.innerHTML = '';
+  onInIt();
+}
+
+// Example: addProduct(); // Uncomment this line to test adding a new product
 
