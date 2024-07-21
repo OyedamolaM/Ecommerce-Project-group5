b93d57c99276773b02b480e98a7950efec35a7ac
Oyedamola Moreira <66420203+OyedamolaM@users.noreply.github.com>
2024-07-21 07:20:51 +0100

    Unified and synchronized landing page javascriptand product array

--- data/products.js
+++ data/products.js
@@ -1,85 +1,157 @@
-//products list for New Arrivals section
-const newArrivals = [{
-    image: 'assets/image 7.png',
-    name: 'T-Shirt with tape details',
-    rating: {
-        stars: 4.5,
-        count: 87,
+const newArrivals =[
+    {
+      id:1,
+      name:  "T-shirt with tape details",
+      image: "/img/n4.png",
+      rating: "4.5/5",
+      price: "100"
     },
-    priceCents: 1090
-
-},
-{
-    image: 'assets/skinny fit jeans.png',
-    name: 'Skinny-fit Jeans',
-    rating: {
-        stars: 4,
-        count: 61,
+    {
+      id:2,
+      name:  "T-shirt with tape details",
+      image: "/img/n2.png",
+      rating: "4.5/5",
+      price: "100"
     },
-    priceCents: 1090
-
-},
-{
-    image: 'assets/Checkered Shirt.png',
-    name: 'Checkered Shirt',
-    rating: {
-        stars: 4,
-        count: 61,
+  
+    {
+      id:3,
+      name:  "T-shirt with tape details",
+      image: "/img/n3.png",
+      rating: "4.5/5",
+      price: "100"
     },
-    priceCents: 1090
-
-},
-{
-    image: 'assets/Sleeve Striped T-Shirt.png',
-    name: 'Sleeve Striped T-Shirt',
-    rating: {
-        stars: 4,
-        count: 61,
+  
+    {
+      id:4,
+      name:  "T-shirt with tape details",
+      image: "/img/n1.png",
+      rating: "4.5/5",
+      price: "100"
     },
-    priceCents: 1090
+  ]
 
-},
-]
 
 //top selling products list
-const topSelling = [{
-    image: 'assets/Vertical Striped Shirt.png',
-    name: 'Vertical-Striped Shirt',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
+const topSelling = [
+    {
+    id:5,
+    name:  "T-shirt with tape details",
+    image: "/img/n5.png",
+    rating: "4.5/5",
+    price: "100"
+  },
 
-},
-{
-    image: 'assets/Courage Graphic T-shirt.png',
-    name: 'Courage Graphic T-Shirt',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
+  {
+    id:6,
+    name:  "T-shirt with tape details",
+    image: "/img/n7.png",
+    rating: "4.5/5",
+    price: "100"
+  },
 
-},
-{
-    image: 'assets/bermuda shorts.png',
-    name: 'Loose-fit Bermuda Shorts',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
+  {
+    id:7,
+    name:  "T-shirt with tape details",
+    image: "/img/n6.png",
+    rating: "4.5/5",
+    price: "100"
+  },
 
-},
-{
-    image: 'assets/Faded Skinny jeans.png',
-    name: 'Faded Skinny Jeans',
-    rating: {
-        stars: 4,
-        count: 61,
-    },
-    priceCents: 1090
-
-},
+  {
+    id:8,
+    name:  "T-shirt with tape details",
+    image: "/img/n8.png",
+    rating: "4.5/5",
+    price: "100"
+  },
 ]
+
+
+
+//HOC PRODUCTS LISTS
+//products list for New Arrivals section
+// const newArrivals = [
+//     {
+//     image: '/assets/image 7.png',
+//     name: 'T-Shirt with tape details',
+//     rating: {
+//         stars: 4.5,
+//         count: 87,
+//     },
+//     priceCents: 1090
+
+// },
+// {
+//     image: '/assets/skinny fit jeans.png',
+//     name: 'Skinny-fit Jeans',
+//     rating: {
+//         stars: 4,
+//         count: 61,
+//     },
+//     priceCents: 1090
+
+// },
+// {
+//     image: '/assets/Checkered Shirt.png',
+//     name: 'Checkered Shirt',
+//     rating: {
+//         stars: 4,
+//         count: 61,
+//     },
+//     priceCents: 1090
+
+// },
+// {
+//     image: '/assets/Sleeve Striped T-Shirt.png',
+//     name: 'Sleeve Striped T-Shirt',
+//     rating: {
+//         stars: 4,
+//         count: 61,
+//     },
+//     priceCents: 1090
+
+// },
+
+//PRODUCTS LISTS FOR TOP SELLING
+// const topSelling = [{
+//     image: '/assets/Vertical Striped Shirt.png',
+//     name: 'Vertical-Striped Shirt',
+//     rating: {
+//         stars: 4,
+//         count: 61,
+//     },
+//     priceCents: 1090
+
+// },
+// {
+//     image: '/assets/Courage Graphic T-shirt.png',
+//     name: 'Courage Graphic T-Shirt',
+//     rating: {
+//         stars: 4,
+//         count: 61,
+//     },
+//     priceCents: 1090
+
+// },
+// {
+//     image: '/assets/bermuda shorts.png',
+//     name: 'Loose-fit Bermuda Shorts',
+//     rating: {
+//         stars: 4,
+//         count: 61,
+//     },
+//     priceCents: 1090
+
+// },
+// {
+//     image: '/assets/Faded Skinny jeans.png',
+//     name: 'Faded Skinny Jeans',
+//     rating: {
+//         stars: 4,
+//         count: 61,
+//     },
+//     priceCents: 1090
+
+// },
+// ]
---
+++ landing page/landing page.js
@@ -0,0 +1,217 @@
+// let newArrivalsHTMl ="";
+NewArrivalHtml = "";
+
+  
+  newArrivals.forEach((product) =>{
+    console.log("");
+      NewArrivalHtml += `
+        <div class="col">
+        <div class="col-img">
+          <img src="${product.image}" alt="tape details">
+        </div>
+        <div class="col-icon">
+          <a href="#"><i class="ri-heart-line"></i></a>
+          <a href="#"><i class="ri-eye-line"></i></a>
+          <a href="#"><i class="ri-shopping-cart-fill"></i></a>
+        </div>
+  </div>`
+  })
+
+
+
+  const body = document.querySelector("body");
+  document.getElementById("new-arrival-products").innerHTML = NewArrivalHtml
+  
+  let topSellingHTML = "";
+  topSelling.forEach((product) =>{
+    console.log("");
+      topSellingHTML += `
+        <div class="col">
+        <div class="col-img">
+          <img src="${product.image}" alt="tape details">
+        </div>
+        <div class="col-icon">
+          <a href="#"><i class="ri-heart-line"></i></a>
+          <a href="#"><i class="ri-eye-line"></i></a>
+          <a href="#"><i class="ri-shopping-cart-fill"></i></a>
+        </div>
+  </div>`
+  })
+
+  document.getElementById("top-selling-products").innerHTML = topSellingHTML
+
+  // let checkOutlist = [];
+  // function onInIt() {
+  //   ArrProducts.forEach((item, key) => {
+  //    let div = document.createElement("div");
+  //     div.classList.add("item");
+  
+  //      div.innerHTML=`
+   
+  //            <img class="img-flud mb-3" src="/img/${item.image}" width="100%">
+  //            <div class="ranting">
+  //              <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+  //              <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+  //              <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+  //              <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+  //              <i class="fa-regular fa-star" style="color: #FFD43B;"></i>
+  //            </div>
+  //            <h5 class="p-name">itailina Shoe</h5>
+  //            <h4 class="p-price">$92.00</h4>
+  //            <button class="buy-btnn">Buy now</button>
+  //           </div>
+  
+  
+  //       `;
+  //      products.appendChild(div);
+  
+  //   });
+  // }
+  // onInIt()
+  
+  
+  function addTocart(Id) {
+    console.log(ArrProducts[Id]);
+    if (checkOutlist[Id] == null) {
+      checkOutlist[Id] = ArrProducts[Id];
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  //HOC LANDING JAVASCRIPT COMMENTED
+  // let NewArrivalProductsHtml = "";
+// // // rendering of New Arrivals section
+// // newArrivals.forEach((product) => {
+// //     NewArrivalProductsHtml +=`
+// //     <div class="col-md-3">
+// //             <div class="thumbnail">
+// //                 <img src='${product.image}' alt="tape details" style="width:100%; height:300px" class="rounded-3">
+// //                 <div class="row">
+// //                     <div class="col-md-12">
+// //                      <span class="label label-primary">${product.name}</span>
+// //                      <div class="row">
+// //                         <div class="col-md-8" style="width: 100;">
+// //                          <span class="sr-only">Four out of Five Stars</span>
+// //                          <span class="glyphicon glyphicon-star"></span>
+// //                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+// //                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+// //                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+// //                          <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+// //                          <span class="label label-success">${product.rating.count}</span>
+// //                         </div>
+// //                        </div>
+// //                        <div class="row">
+// //                         <div class="col-md-6">
+// //                             <h5 class="product-price">$${(product.priceCents /100).toFixed(2)}</h5>
+// //                         </div>
+// //                        </div>
+// //                        <div class="row">
+// //                             <div class="col-md-12">
+// //                                 <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">Add to cart</button>
+// //                             </div>
+// //                         </div>
+// //                     </div>
+// //             </div>
+// //             </div>
+// //           </div>
+// //     `
+// // //  
+
+            
+// // })
+
+// // document.querySelector('.new-arrivals-grid').innerHTML = NewArrivalProductsHtml; 
+
+// document.querySelectorAll('.js-add-to-cart')
+//     .forEach((button) => {
+//             button.addEventListener("click", () =>{
+//                 console.log(button.dataset);
+//             });
+//     }
+
+//     );
+
+
+
+// let topSellingHTML = "";
+// // rendering of New Top selling section
+// topSelling.forEach((product) => {
+//     topSellingHTML +=`
+//     <div class="col-md-3">
+//             <div class="thumbnail">
+//                 <img src='${product.image}' alt="tape details" style="width:100%; height:300px" class="rounded-3">
+//                 <div class="row">
+//                     <div class="col-md-12">
+//                      <span class="label label-primary">${product.name}</span>
+//                      <div class="row">
+//                         <div class="col-md-8" style="width: 100;">
+//                          <span class="sr-only">Four out of Five Stars</span>
+//                          <span class="glyphicon glyphicon-star"></span>
+//                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+//                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+//                          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+//                          <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+//                          <span class="label label-success">${product.rating.count}</span>
+//                         </div>
+//                        </div>
+                    
+//                        <div class="row">
+//                         <div class="col-md-6">
+//                             <h5 class="product-price">$${product.priceCents /100}</h5>
+//                         </div>
+//                        </div>
+//                        <div class="row">
+//                             <div class="col-md-12">
+//                                 <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">Add to cart</button>
+//                             </div>
+//                         </div>
+//                     </div>
+//             </div>
+//             </div>
+//           </div>
+//     `
+// //  
+
+            
+// })
+
+// document.querySelector('.top-selling').innerHTML = topSellingHTML; 
+
+
+// document.querySelectorAll('.js-add-to-cart')
+//     .forEach((button) => {
+//             button.addEventListener("click", () =>{
+//                 const productName = button.dataset.productName
+//                 cart.push({
+//                     productName: productName,
+//                     quantity: 1
+//                 });
+//                 console.log(cart);
+//             });
+//     }
+
+//     );
