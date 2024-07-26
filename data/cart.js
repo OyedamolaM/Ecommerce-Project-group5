7047a10820be42fa1bbe626c4ef85f02b9700da4
Oyedamola Moreira <66420203+OyedamolaM@users.noreply.github.com>
2024-07-26 05:34:24 +0100

    add to cart perfected

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,5 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
-// import { newArrivals } from "./products";
-
  if(!cart){
 let cart = [
         ];
@@ -22,39 +20,12 @@ export function addToCart(productId) {
       cart.forEach((cartItem)=>{
           if (productId === cartItem.productId){
             matchingItem = cartItem;
-            // console.log(matchingItem.productId);
-            // console.log(cartItem);
+            
             matchingItemId = matchingItem.productId
           } 
           console.log(matchingItemId);
-          
-          
-          // let matchingProduct;
-          // newArrivals.forEach((products) => {
-          //   if (products.id = matchingItemId){
-          //       matchingProduct = products
-          //   }
-          //   console.log(matchingProduct);
-          // }
-          // )
-          
-          
-          
-          // console.log(matchingItemId);
-          // cart.forEach((cartProduct) =>{
-          //   //productId is asigned variable to access each of the items id within the cart
-          //   const productId = cartProduct.productId
-            
-          //    let matchingProduct;
-          //   // topSelling,
-          //   newArrivals.forEach((products)=>{
-          //       if (products.id === productId){
-          //           matchingProduct = products
-          //       }
-          //       console.log(matchingProduct);
-                
-          // })
-      });
+      
+        });
       if(matchingItem){
             matchingItem.quantity +=1
           }
@@ -83,19 +54,3 @@ if (cartItem.productId !== productIdToBeDeleted){
 
     CartLocalStorage();
   }
-
-  
-
-//   export function removeCart(productId) {
-
-
-//     cart.forEach((cartItem) =>{
-// if (cartItem.productId !== productId){
-//     console.log(productId);
-//     console.log(cartItem.productId);
-   
-//     cart.pop(productId);
-// }
-//     }
-// );
-// 
